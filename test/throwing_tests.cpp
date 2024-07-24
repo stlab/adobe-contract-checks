bef3e6bf2d2b88c38e6b13173d0570fdf68529d5
Dave Abrahams <dabrahams@adobe.com>
2024-07-24 15:25:11 -0700

    Use lambdas to avoid undocumentqble named function definition.

--- test/throwing_tests.cpp
+++ test/throwing_tests.cpp
@@ -6,14 +6,8 @@
   throw reason;
 }
 
-namespace {
-
-void precondition_check(bool pass) { ADOBE_PRECONDITION(pass); }
-
-}// namespace
-
 TEST_CASE("Throwing violation handler works", "[throwing]")
 {
-  CHECK_THROWS_AS(precondition_check(false), adobe::contract_violation);
-  CHECK_THROWS_WITH(precondition_check(false), "precondition");
+  CHECK_THROWS_AS([]{ ADOBE_PRECONDITION(false); }(), adobe::contract_violation);
+  CHECK_THROWS_WITH([]{ ADOBE_PRECONDITION(false); }(), "precondition");
 }
