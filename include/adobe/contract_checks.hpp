ef8259a599c2d1314c49654ed9cbfc3ca0c43d88
Dave Abrahams <dabrahams@adobe.com>
2024-07-23 17:41:11 -0700

    Header improvements.

--- include/adobe/contract_checks.hpp
+++ include/adobe/contract_checks.hpp
@@ -8,11 +8,11 @@ namespace adobe {
 
   class contract_violation final: public ::std::logic_error {
   private:
-    const std::error_code _code;
+    std::error_code _code;
   public:
-    contract_violation(const char* message) : ::std::logic_error(message) {}
+    explicit contract_violation(const char *message) : ::std::logic_error(message) {}
 
-    const std::error_code& code() const noexcept {
+    [[nodiscard]] const std::error_code& code() const noexcept {
       return _code;
     }
   };
@@ -20,6 +20,9 @@ namespace adobe {
   [[noreturn]] void contract_violated(contract_violation const& reason);
 
   [[noreturn]] inline void default_contract_violated(contract_violation const& reason) {
+    // This pattern, calling terminate while unwinding, causes most
+    // standard libraries to report the exception that was thrown via
+    // a default terminate handler.
     try {
       throw reason;
     } catch(...) {
@@ -28,11 +31,16 @@ namespace adobe {
   }
 }
 
+// TODO: Supply a default terminate handler that calls
+// get_current_exception and reports info.
+
 #define ADOBE_DEFAULT_CONTRACT_VIOLATION_HANDLER() \
   [[noreturn]] void ::adobe::contract_violated(::adobe::contract_violation const& reason) \
   { ::adobe::default_contract_violated(reason); }
 
-#define ADOBE_PRECONDITION(condition) if(condition); else ::adobe::contract_violated(::adobe::contract_violation{"precondition"})
+#define ADOBE_PRECONDITION(condition) \
+  if(condition); else \
+    ::adobe::contract_violated(::adobe::contract_violation{"precondition"})
 
 #define ADOBE_POSTCONDITION(condition)
 #define ADOBE_INVARIANT(condition)
