b4ac36bfeb9ef241aa783d6a2adf03936373dd2b
Dave Abrahams <dabrahams@adobe.com>
2024-07-24 17:09:32 -0700

    Test that file and line information is encoded in contract_violation.

--- test/throwing_tests.cpp
+++ test/throwing_tests.cpp
@@ -1,13 +1,28 @@
+#include "adobe/contract_checks.hpp"
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers.hpp>
-#include "adobe/contract_checks.hpp"
+#include <cstdint>
+#include <string_view>
 
-[[noreturn]] void ::adobe::contract_violated(::adobe::contract_violation const& reason) {
+[[noreturn]] void ::adobe::contract_violated(::adobe::contract_violation const &reason)
+{
   throw reason;
+}
+
+TEST_CASE("Precondition violation encodes file and line", "[file-and-line-encoded]")
+{
+  std::uint32_t expected_line = 0;
+  try {
+    expected_line = __LINE__ + 1;
+    ADOBE_PRECONDITION(false);
+  } catch (const adobe::contract_violation &v) {
+    CHECK((v.line() == expected_line));
+    CHECK((std::string_view(v.file()) == __FILE__));
+  }
 }
 
 TEST_CASE("Throwing violation handler works", "[throwing]")
 {
-  CHECK_THROWS_AS([]{ ADOBE_PRECONDITION(false); }(), adobe::contract_violation);
-  CHECK_THROWS_WITH([]{ ADOBE_PRECONDITION(false); }(), "precondition");
+  CHECK_THROWS_AS([] { ADOBE_PRECONDITION(false); }(), adobe::contract_violation);
+  CHECK_THROWS_WITH([] { ADOBE_PRECONDITION(false); }(), "precondition");
 }
