0a80a3d33e1018d0849662010df731b29bae1eed
Dave Abrahams <dabrahams@adobe.com>
2024-08-12 15:51:02 -0700

    Nix the signal handler to see if it helps our failing Windows case.

--- test/precondition_tests.cpp
+++ test/precondition_tests.cpp
@@ -8,12 +8,11 @@ ADOBE_DEFAULT_CONTRACT_VIOLATION_HANDLER()
 
 #ifdef _WIN32
 // NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
-#define EXPECT_ABORT(code, expected_output_regex) \
-  EXPECT_DEATH(code, expected_output_regex ".*\n*##ABORTED##");
+#define EXPECT_ABORT(code, expected_output_regex) EXPECT_DEATH(code, expected_output_regex);
 #else
 // NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
 #define EXPECT_ABORT(code, expected_output_regex) \
-  EXPECT_EXIT(code, testing::KilledBySignal(SIGABRT), expected_output_regex ".*##ABORTED##");
+  EXPECT_EXIT(code, testing::KilledBySignal(SIGABRT), expected_output_regex);
 #endif
 
 TEST(PreconditionDeathTest, PreconditionFailureAborts)
