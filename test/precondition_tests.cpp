f6aeb9255eb3ab9b9014183eade5d4ce13ca024c
Dave Abrahams <dabrahams@adobe.com>
2024-08-12 13:34:44 -0700

    Try using a different death test for _WIN32

--- test/precondition_tests.cpp
+++ test/precondition_tests.cpp
@@ -9,19 +9,27 @@ TEST(PreconditionDeathTest, PreconditionFailureAborts)
   EXPECT_EXIT(ADOBE_PRECONDITION(false), testing::KilledBySignal(SIGABRT), "precondition");
 }
 
+#ifdef _WIN32
+// NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
+#define EXPECT_ABORT(code, expected_output_regex) \
+  EXPECT_DEATH(code, expected_output_regex ".*##ABORTED##");
+#else
+// NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
+#define EXPECT_ABORT(code, expected_output_regex) \
+  EXPECT_EXIT(code, testing::KilledBySignal(SIGABRT), expected_output_regex ".*##ABORTED##");
+#endif
+
 TEST(PreconditionDeathTest, PreconditionFailureOutput)
 {
-#line 9999
-  EXPECT_EXIT(ADOBE_PRECONDITION(false),
-    testing::KilledBySignal(SIGABRT),
-    "precondition_tests.cpp:9999: Precondition violated [(]false[)]");
+#line 9998
+  EXPECT_ABORT(
+    ADOBE_PRECONDITION(false), "precondition_tests.cpp:9999: Precondition violated [(]false[)]");
 }
 
 TEST(PreconditionDeathTest, PreconditionFailureWithMessageOutput)
 {
 #line 99991
-  EXPECT_EXIT(ADOBE_PRECONDITION(false, "expected message"),
-    testing::KilledBySignal(SIGABRT),
+  EXPECT_ABORT(ADOBE_PRECONDITION(false, "expected message"),
     "precondition_tests.cpp:99991: Precondition violated [(]false[)]. "
     "expected message");
 }
