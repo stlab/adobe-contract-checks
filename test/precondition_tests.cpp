450bead343a587eeab85bae749398fab9dfafc46
Dave Abrahams <dabrahams@adobe.com>
2024-08-12 14:12:23 -0700

    Work around GTest Windows regex differences.
    https://github.com/google/googletest/blob/ff233bdd4cac0a0bf6e5cd45bda3406814cb2796/docs/advanced.md#regular-expression-syntax

--- test/precondition_tests.cpp
+++ test/precondition_tests.cpp
@@ -9,30 +9,32 @@ ADOBE_DEFAULT_CONTRACT_VIOLATION_HANDLER()
 #ifdef _WIN32
 // NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
 #define EXPECT_ABORT(code, expected_output_regex) \
-  EXPECT_DEATH(code, expected_output_regex "(.|\\n|\\r)*##ABORTED##");
+  EXPECT_DEATH(code, expected_output_regex ".*\\n*##ABORTED##");
+// NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
+#define FALSE_CONDITION() " (false)"
 #else
 // NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
 #define EXPECT_ABORT(code, expected_output_regex) \
-  EXPECT_EXIT(                                    \
-    code, testing::KilledBySignal(SIGABRT), expected_output_regex "(.|\\n|\\r)*##ABORTED##");
+  EXPECT_EXIT(code, testing::KilledBySignal(SIGABRT), expected_output_regex ".*##ABORTED##");
+// NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
+#define FALSE_CONDITION() " \\(false\\)"
 #endif
 
 TEST(PreconditionDeathTest, PreconditionFailureAborts)
 {
-  EXPECT_ABORT(ADOBE_PRECONDITION(false), "precondition");
+  EXPECT_ABORT(ADOBE_PRECONDITION(false), "Precondition violated" FALSE_CONDITION());
 }
 
 TEST(PreconditionDeathTest, PreconditionFailureOutput)
 {
-#line 9998
-  EXPECT_ABORT(
-    ADOBE_PRECONDITION(false), "precondition_tests.cpp:9999: Precondition violated [(]false[)]");
+#line 9999
+  EXPECT_ABORT(ADOBE_PRECONDITION(false),
+    "precondition_tests.cpp:9999: Precondition violated" FALSE_CONDITION());
 }
 
 TEST(PreconditionDeathTest, PreconditionFailureWithMessageOutput)
 {
 #line 99991
   EXPECT_ABORT(ADOBE_PRECONDITION(false, "expected message"),
-    "precondition_tests.cpp:99991: Precondition violated [(]false[)]. "
-    "expected message");
+    "precondition_tests.cpp:99991: Precondition violated" FALSE_CONDITION() ".*expected message");
 }
