a42a1f7caa584a89c84f0923aa37fdcaba38395a
Dave Abrahams <dabrahams@adobe.com>
2024-08-12 16:30:18 -0700

    Restore the expected message.

--- test/precondition_tests.cpp
+++ test/precondition_tests.cpp
@@ -30,6 +30,6 @@ TEST(PreconditionDeathTest, PreconditionFailureOutput)
 TEST(PreconditionDeathTest, PreconditionFailureWithMessageOutput)
 {
 #line 9991
-  EXPECT_ABORT(ADOBE_PRECONDITION(false, ""),
-    "precondition_tests.cpp:999.: Precondition violated \\(false\\)\\.");
+  EXPECT_ABORT(ADOBE_PRECONDITION(false, "expected message"),
+    "precondition_tests.cpp:999.: Precondition violated \\(false\\)\\. expected message");
 }
